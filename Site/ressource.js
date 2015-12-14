448b301f3064f999291d3d487650d519b67444a0
simplon-jimmypo <poirot.jimmy@gmail.com>
2015-12-14 15:35:06 +0100

    ajout de ressource + modif photo amel

---
+++ Site/ressource.js
@@ -0,0 +1,9 @@
+
+function info(i){
+  var hiddenLi = document.getElementById("hiddenLi"+i);
+  hiddenLi.style.display = "inline-block";
+  event.target.addEventListener("mouseout", efface);
+  function efface(){
+    hiddenLi.style.display = "none";
+  }
+}
