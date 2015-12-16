fa7d65a7f0e88775cb6456ff4be2a0bd556eaf25
simplon-benoitd <benoitdelboe@hotmail.fr>
2015-12-16 14:36:53 +0100

    remise bon lien home - premiers pas

--- Fini_Site/Site/exemples.js
+++ Fini_Site/Site/exemples.js
@@ -9,7 +9,7 @@ function info(i){
 }
 
 
-  var tableauDeSites = ["http://127.0.0.1:8080/pendu.html","http://andrew-hoyer.com/experiments/cloth/","http://www.professorcloud.com/mainsite/canvas-nebula.htm","http://hakim.se/experiments/html5/blob/03/","http://9elements.com/io/projects/html5/canvas/"];
+  var tableauDeSites = ["http://127.0.0.1:8080/pendu.html","#","http://andrew-hoyer.com/experiments/cloth/","http://www.professorcloud.com/mainsite/canvas-nebula.htm","http://hakim.se/experiments/html5/blob/03/","http://9elements.com/io/projects/html5/canvas/"];
 
   function affiche(i){
     var iframe = document.getElementById("iframe");
